7c3256b950357faa446ecf6bb71bbb2c7fb286bf
Nyrmin <nyrminoulkaid75@gmail.com>
2024-10-18 10:22:35 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,14 +12,20 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 def est_authentifie():
     return session.get('authentifie')
 
+def user():
+    return session.get('user_A')
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
 
 @app.route('/lecture')
 def lecture():
-    if not est_authentifie():
-        # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
+    if est_authentifie():
+        return "<h1>Bonjour Administrateur</h1>"
+    elif user():
+        return "<h1>Bonjour Utilisateur</h1>"
+    else:
         return redirect(url_for('authentification'))
 
   # Si l'utilisateur est authentifié
@@ -31,7 +37,12 @@ def authentification():
         # Vérifier les identifiants
         if request.form['username'] == 'admin' and request.form['password'] == 'password': # password à cacher par la suite
             session['authentifie'] = True
+            session['user_A'] = False
             # Rediriger vers la route lecture après une authentification réussie
+            return redirect(url_for('lecture'))
+        elif request.form['username'] == 'user' and request.form['password'] == '12345':
+            session['user_A'] = True
+            session['authentifie'] = False
             return redirect(url_for('lecture'))
         else:
             # Afficher un message d'erreur si les identifiants sont incorrects
@@ -39,11 +50,11 @@ def authentification():
 
     return render_template('formulaire_authentification.html', error=False)
 
-@app.route('/fiche_nom/<string:id>')
-def Readfiche(id):
+@app.route('/fiche_client/<int:post_id>')
+def Readfiche(post_id):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE nom = ?', (id,))
+    cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
     data = cursor.fetchall()
     conn.close()
     # Rendre le template HTML et transmettre les données
@@ -76,6 +87,34 @@ def enregistrer_client():
     conn.commit()
     conn.close()
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
+
+@app.route('/fiche_nom/<string:nom>')
+def fiche_nom(nom):
+    if user():
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        cursor.execute('SELECT * FROM clients WHERE nom = ?', (nom,))
+        data = cursor.fetchall()
+        conn.close
+        return render_template('read_data.html', data=data)
+    else:
+        return '<h1>non identifié</h1>'
+
+
+@app.route('/consultation_livre')
+def BDD_livre():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM livres;')
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('read_data.html', data=data)
+
+@app.route('/enregistrer_livre', methods=['GET'])
+def formulaire_livre():
+    return render_template('formulaire_livre.html')  # afficher le formulaire
+
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
